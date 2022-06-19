6ee7d1ea765e049ed4fdbdd1593b81975b346762
nswon <swnam0729@gmail.com>
2022-06-20 00:24:04 +0900

    SOVLE :: 수 정렬하기 - https://www.acmicpc.net/problem/2750

--- algorithm/src/day8/Boj_2750.java
+++ algorithm/src/day8/Boj_2750.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Boj_2750 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> l = new ArrayList<>();
