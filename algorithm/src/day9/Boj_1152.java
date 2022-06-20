25b78374917fc0dd4c8bdbe2212771e9590ba10c
nswon <swnam0729@gmail.com>
2022-06-20 20:23:09 +0900

    FAIL :: 단어의 개수 - https://www.acmicpc.net/problem/1152 - 왜 틀렸는지 모르겠음

---
+++ algorithm/src/day9/Boj_1152.java
@@ -0,0 +1,14 @@
+package day9;
+
+import java.util.*;
+
+public class Boj_1152 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        s = s.trim();
+        String[] arr = s.split(" ");
+
+        System.out.println(arr.length);
+    }
+}
