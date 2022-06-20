9602002800764b6085ccf464590cc6fe0154fd2c
nswon <swnam0729@gmail.com>
2022-06-20 21:54:56 +0900

    SOVLE :: 단어의 개수 https://www.acmicpc.net/problem/1152

--- algorithm/src/day9/Boj_1152.java
+++ algorithm/src/day9/Boj_1152.java
@@ -1,14 +1,17 @@
 package day9;
 
+import java.io.*;
 import java.util.*;
 
 public class Boj_1152 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        s = s.trim();
-        String[] arr = s.split(" ");
 
-        System.out.println(arr.length);
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String s = bf.readLine();
+        s = s.trim();
+
+        StringTokenizer st = new StringTokenizer(s);
+
+        System.out.println(st.countTokens());
     }
 }
