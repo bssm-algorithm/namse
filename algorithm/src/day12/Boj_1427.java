7fb456237ae66f8923fdbdf1c2c2fb3d4d526493
nswon <swnam0729@gmail.com>
2022-06-22 13:43:14 +0900

    SOVLE :: 소트인사이드 - https://www.acmicpc.net/problem/1427

---
+++ algorithm/src/day12/Boj_1427.java
@@ -0,0 +1,21 @@
+package day12;
+
+import java.util.*;
+
+public class Boj_1427 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String  s = sc.next();
+
+        List<String> list = new ArrayList<>(Arrays.asList(s.split("")));
+
+        Collections.sort(list);
+        Collections.reverse(list);
+
+        for(int i=0; i<list.size(); i++) {
+            System.out.print(list.get(i));
+
+        }
+    }
+}
