5e567b5f831c4952b96af70ab7abef9bd7730de2
nswon <swnam0729@gmail.com>
2022-06-25 19:24:53 +0900

    REFACTOR :: 숫자의 개수 - https://www.acmicpc.net/problem/2577 - 아스키코드를 이용해서 푸는 연습좀 해야겠다

--- algorithm/src/day15/Boj_2577.java
+++ algorithm/src/day15/Boj_2577.java
@@ -1,9 +1,30 @@
 package day15;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Boj_2577 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[10];
+        int mul = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+
+        String s = String.valueOf(mul);
+
+        for(int i=0; i<s.length(); i++) {
+            arr[s.charAt(i) - 48]++;
+        }
+
+        for(int j : arr) {
+            System.out.print(arr[j]);
+        }
+
+/*
+        내코드
+
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
@@ -20,5 +41,7 @@ public class Boj_2577 {
         for(int i=0; i<=9; i++) {
                     System.out.println(Collections.frequency(number, i));
         }
+
+ */
     }
 }
