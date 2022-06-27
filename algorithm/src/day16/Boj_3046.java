f1cdedc40cd54e05ec2fbe668652aa9cf61da571
nswon <swnam0729@gmail.com>
2022-06-27 21:48:36 +0900

    SOVLE :: R2 - https://www.acmicpc.net/problem/3046

---
+++ algorithm/src/day16/Boj_3046.java
@@ -0,0 +1,19 @@
+package day16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_3046 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int r1 = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int r2 = (s*2)-r1;
+        System.out.println(r2);
+    }
+}
