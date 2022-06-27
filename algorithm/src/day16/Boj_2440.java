0a93297e7b8ae2d250393513c22628ba6457501c
nswon <swnam0729@gmail.com>
2022-06-27 15:08:49 +0900

    SOVLE :: 별 찍기 - 3 - https://www.acmicpc.net/problem/2440

---
+++ algorithm/src/day16/Boj_2440.java
@@ -0,0 +1,19 @@
+package day16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_2440 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=n; i>0; i--) {
+            for(int j=i; j>0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
