727c451c862bdf0d63d52bab49f38caa01e1af2a
nswon <swnam0729@gmail.com>
2022-06-28 08:40:57 +0900

    FAIL :: 쉽게 푸는 문제 - https://www.acmicpc.net/problem/1292

---
+++ algorithm/src/day17/Boj_2953.java
@@ -0,0 +1,23 @@
+package day17;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_2953 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        for(int i=a+1; i<b; i++) {
+            for(int j=0; j<i; j++) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
