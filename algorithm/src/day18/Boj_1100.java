e638e1cb8f865f7c3f7a1d5e8ee48867d520e73c
nswon <swnam0729@gmail.com>
2022-06-29 22:26:39 +0900

    REFATOR :: 하얀 칸 - https://www.acmicpc.net/problem/1100

--- algorithm/src/day18/Boj_1100.java
+++ algorithm/src/day18/Boj_1100.java
@@ -8,23 +8,13 @@ public class Boj_1100 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int cnt = 0;
-        int temp = 1;
 
         for(int i=0; i<8; i++) {
             String s = br.readLine();
 
-            if(i%2==1) {
-                for(int j=0; j<8; j++) {
-                    if(j%2==1 && s.charAt(j) == 'F') {
-                        cnt++;
-                    }
-                }
-            }
-            else {
-                for(int j=0; j<8; j++) {
-                    if(j%2==0 && s.charAt(j) == 'F') {
-                        cnt++;
-                    }
+            for(int j=0; j<8; j++) {
+                if((i+j)%2==0 && s.charAt(j) == 'F') {
+                    cnt++;
                 }
             }
         }
