61a94fe205fe1b6ba0b61695a7aef86b89cff582
nswon <swnam0729@gmail.com>
2022-07-02 17:24:04 +0900

    REFATOR :: 하얀 칸 - https://www.acmicpc.net/problem/1100 - 전에 내가 짰던 코드 추가

--- algorithm/src/day18/Boj_1100.java
+++ algorithm/src/day18/Boj_1100.java
@@ -4,6 +4,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*      내코드 추가
+        역시 수학적 머리가 필요함
+        하얀칸 : (0,0) (0,2) ... (1,1) (1,3)
+        저걸 보고 i+j가 짝수면 되구나라는 걸 어떻게 알까
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        int temp = 1;
+
+        for(int i=0; i<8; i++) {
+            String s = br.readLine();
+
+            if(i%2==1) {
+                for(int j=0; j<8; j++) {
+                    if(j%2==1 && s.charAt(j) == 'F') {
+                        cnt++;
+                    }
+                }
+            }
+            else {
+                for(int j=0; j<8; j++) {
+                    if(j%2==0 && s.charAt(j) == 'F') {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+ */
+
 public class Boj_1100 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
