9ebbd64d363282cb842792586e1395aab811e41a
nswon <swnam0729@gmail.com>
2022-07-06 00:09:32 +0900

    SOVLE :: 인생 점수 - https://www.acmicpc.net/problem/15351

---
+++ algorithm/src/day25/Boj_15351.java
@@ -0,0 +1,30 @@
+package day25;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_15351 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++) {
+            String s = br.readLine();
+            int sum = 0;
+
+            s = s.replace(" ", "");
+
+            for(int j=0; j<s.length(); j++) {
+                sum += (s.charAt(j) - 'A') + 1;
+            }
+
+            if(sum == 100) {
+                System.out.println("PERFECT LIFE");
+            }
+            else {
+                System.out.println(sum);
+            }
+        }
+    }
+}
