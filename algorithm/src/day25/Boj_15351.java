00e5ae2262799a2fe14848336071f8f9728a9395
nswon <swnam0729@gmail.com>
2022-07-06 00:12:28 +0900

    REFATOR :: 인생 점수 - https://www.acmicpc.net/problem/15351 - 컨벤션 지키기

--- algorithm/src/day25/Boj_15351.java
+++ algorithm/src/day25/Boj_15351.java
@@ -9,20 +9,19 @@ public class Boj_15351 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             String s = br.readLine();
             int sum = 0;
 
             s = s.replace(" ", "");
 
-            for(int j=0; j<s.length(); j++) {
+            for (int j = 0; j < s.length(); j++) {
                 sum += (s.charAt(j) - 'A') + 1;
             }
 
-            if(sum == 100) {
+            if (sum == 100) {
                 System.out.println("PERFECT LIFE");
-            }
-            else {
+            } else {
                 System.out.println(sum);
             }
         }
