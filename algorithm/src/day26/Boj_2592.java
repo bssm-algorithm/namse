dfe4df0a1cd1f2c924d849004219fd65748c1927
nswon <swnam0729@gmail.com>
2022-07-07 23:25:41 +0900

    SOVLE :: 대표값 - https://www.acmicpc.net/problem/2592

---
+++ algorithm/src/day26/Boj_2592.java
@@ -0,0 +1,35 @@
+package day26;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Boj_2592 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        int max = 0;
+        int ans = 0;
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<10; i++) {
+            int n = Integer.parseInt(br.readLine());
+            list.add(n);
+
+            sum += n;
+        }
+
+        for(int i=0; i<10; i++) {
+            if(Collections.frequency(list, list.get(i)) > max) {
+                max = Collections.frequency(list, list.get(i));
+                ans = list.get(i);
+            }
+        }
+
+        System.out.println(sum/10);
+        System.out.println(ans);
+    }
+}
